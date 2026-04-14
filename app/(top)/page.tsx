e0e7d6d879567895dd12b3d34cc5e91f72f1cd2b
ゆう君 <yuuichidesu2018@gmail.com>
2026-04-15 07:55:24 +0900

    データ追加

--- app/(top)/page.tsx
+++ app/(top)/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           <p className="text-xl text-gray-800 md:text-3xl md:leading-normal">
-            <strong>This is an app to share our beloved son`s photos</strong>
+            <strong>This is an app to share our beloved son`s photos! I'm excited to begin!</strong>
             <br />
             We are going to share his cutness together!
           </p>
