6a78b5e1994c9252a7b488af65ee95fb580c8f90
伊地知 侑一 <ki00215@kyowa.com>
2026-04-21 08:15:04 +0900

    新着投稿画面

--- lib/apis.ts
+++ lib/apis.ts
@@ -22,3 +22,27 @@ export async function fetchDashboard() {
     throw new Error("Failed to fetch.");
   }
 }
+
+export async function fetxhLatestPosts(){
+  try{
+    return await prisma.post.findMay({
+      select:{
+        id: true,
+        image: true,
+        caption: true,
+        createdAt: true,
+        user: {
+          select:{
+            id: true,
+            name: true,
+            image: true,
+          },
+        },
+      },
+      orderBy: { createdAt: "desc" },
+    });
+  }catch(error){
+    console.error("Database Error:", error);
+    throw new Error("Failed to fetch posts.");
+  }
+}
